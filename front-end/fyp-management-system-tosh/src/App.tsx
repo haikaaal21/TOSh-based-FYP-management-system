e1a948a91feb249fe04cda939cc63aa6cf7c23e9
Muhammad Haikal <98247943+haikaaal21@users.noreply.github.com>
2023-10-23 07:41:53 +0800

    Initial Commit and React Setup

---
+++ front-end/fyp-management-system-tosh/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+const title = 'FYP Management System';
+const subjects: string[] = ['React', 'TypeScript', 'Vite', 'Turtles ?'];
+let x = 0;
+function App() {
+  
+  const [subject, setSubject] = useState(subjects[x]);
+  const handleClick = () => {
+    console.log(x);
+    if(x < subjects.length - 1) {
+      setSubject(subjects[++x]);
+    } else {
+      setSubject(subjects[x = 0]);
+    }
+  };
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>I like {subject}</p>
+      <button onClick={handleClick}>Click me!</button>
+    </>
+  )
+}
+
+export default App
