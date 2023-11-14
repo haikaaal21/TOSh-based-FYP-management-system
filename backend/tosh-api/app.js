8612c19f675886fce2b91c32c827183344f7b2e1
Muhammad Haikal <98247943+haikaaal21@users.noreply.github.com>
2023-11-14 20:52:08 +0800

    express API

--- backend/tosh-api/app.js
+++ backend/tosh-api/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+
 app.use(express.json());
+app.use(router);
+
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log("Server Listening on PORT:", port);
 })
 
+router.get('/', (req, res) => {
+    res.send("Hello World");
+});
