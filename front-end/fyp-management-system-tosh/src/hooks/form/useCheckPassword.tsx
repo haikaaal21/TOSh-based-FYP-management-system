21b3c869b3533e924eecfc57c3896c12b8c99b8d
Muhammad Haikal <haikaluwu21@gmail.com>
2024-06-20 14:38:34 +0800

    Bug fixes

--- front-end/fyp-management-system-tosh/src/hooks/form/useCheckPassword.tsx
+++ front-end/fyp-management-system-tosh/src/hooks/form/useCheckPassword.tsx
@@ -8,7 +8,7 @@ export const useCheckPassword = () => {
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
     if (!regex.test(password)) {
       errors.password =
-        'Password must contain at least 8 characters, including 1 uppercase letter, 1 lowercase letter, and 1 number';
+        'Password must contain at least 8 characters, including 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number';
     }
     if (password !== confirmPassword) {
       errors.confirmPassword = 'Passwords do not match';
